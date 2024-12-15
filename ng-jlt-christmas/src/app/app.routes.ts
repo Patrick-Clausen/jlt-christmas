675ee0d1ab07701a9850e74d8a32ce600dd78658
Patrick Clausen <patrick@tourcompass.com>
2024-12-15 21:26:15 +0100

    Add welcome

--- ng-jlt-christmas/src/app/app.routes.ts
+++ ng-jlt-christmas/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import {WordleComponent} from './wordle/wordle.component';
 import {PersonQuizComponent} from './person-quiz/person-quiz.component';
 import {SkatteJagtComponent} from './skatte-jagt/skatte-jagt.component';
 import {ResultComponent} from './result/result.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'skatte-jagt', pathMatch: 'full'},
+  {path: '', component: WelcomeComponent},
   {path: 'wordle', component: WordleComponent},
   {path: 'person-quiz', component: PersonQuizComponent},
   {path: 'skatte-jagt', component: SkatteJagtComponent},
